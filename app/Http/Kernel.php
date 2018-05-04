4f24939a998c891789c6e3c88e892ffc6bf46724
David Robison <mrdavidrobison@gmail.com>
2018-05-04 14:00:44 -0600

    add additional controllers

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,6 +59,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'Role' => \App\Http\Middleware\RoleMiddleware::class,
+        'role' => \App\Http\Middleware\RoleMiddleware::class,
+        'terminate' => \App\Http\Middleware\TerminateMiddleware::class,
     ];
 }
