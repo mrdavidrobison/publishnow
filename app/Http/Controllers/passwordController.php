a8305a82b9b1f24334f12d2afe04d4a17c9a88d2
David Robison <mrdavidrobison@gmail.com>
2018-05-16 16:57:16 -0600

    initialize password hashing for storage

---
+++ app/Http/Controllers/passwordController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller
+
+class passwordController extends Controller
+{
+    /**
+     * Updating the password for the user.
+     *
+     * @param Request $request
+     * @return Response
+     */
+
+    public function update(Request $request)
+    {
+      // Validate the new password length...
+        $request->user()->fill([
+            'password' => Hash::make($request->newPassword) // Hashing passwords
+        ])->save();
+
+        if (Hash::check('plain-text', $hashedPassword)) {
+            // The passwords match...
+         }
+    }
