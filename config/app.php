0bccd7fdbe879ea109f53831b01afe4e646b1ed2
David Robison <mrdavidrobison@gmail.com>
2018-05-16 16:30:06 -0600

    create event listeners

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-    'log' => 'daily',
+    'log' => env('APP_LOG','daily'),
     'name' => env('APP_NAME', 'Laravel'),
 
     /*
