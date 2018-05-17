00e0a356f88683e2b65c4ab4177c353cf9365a5f
David Robison <mrdavidrobison@gmail.com>
2018-05-17 14:56:43 -0600

    transfer script tag to index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PublishNow</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 </head>
 <body>
-    
+<script>
+    var app = new Vue({
+        el: "#addbooks",
+        data: {
+            newBooks: '',
+            books: [
+                {title: 'Airborne', published: false, intro: lorem ipsum baby!},
+                {title: 'New Odds Down Under', published: true, intro: lorem ipsum dude! },
+                {title: 'People Shmeeple', published: true, intro: lorem ipsum dudette! }
+            ],
+            buttonTitle: {
+                button1: 'Go ahead....add a book.',
+            },
+            classNames: {
+                red: 'red-font',
+                blue: 'blue-font',
+                dark: 'is-dark',
+                primary: 'is-primary',
+                link: 'is-link',
+                info: 'is-info',
+                success: 'is-success',
+                warning: 'is-warning',
+                danger: 'is-danger',
+            },
+            message: 'Hello World'
+        },
+        methods: {
+            addBook() {
+                this.books.push(this.newBooks);
+                this.newBooks = '';
+            }
+        },
+    });
+</script>    
 </body>
-</html>
